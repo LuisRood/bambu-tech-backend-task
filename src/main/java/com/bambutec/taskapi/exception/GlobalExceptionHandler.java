26f346d7a161b5db5bf614036b6f161d8aa9975b
Luis Felipe Rodríguez Reyes <luisrood69@gmail.com>
2026-04-05 13:44:09 -0600

    1.0.0v Fix: Se solucionaron vulnerabilidades definiendo Strings estaticos

--- src/main/java/com/bambutec/taskapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bambutec/taskapi/exception/GlobalExceptionHandler.java
@@ -10,36 +10,38 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String ERROR = "Error";
+    private static final String MESSAGE = "Message";
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNotFound(ResourceNotFoundException ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", "Not Found");
-        error.put("message", ex.getMessage());
+        error.put(ERROR, "Not Found");
+        error.put(MESSAGE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<Map<String, String>> handleForbidden(ForbiddenException ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", "Forbidden");
-        error.put("message", ex.getMessage());
+        error.put(ERROR, "Forbidden");
+        error.put(MESSAGE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Map<String, String>> handleAuthenticationExcept(AuthenticationException ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", "Authentication Failed");
-        error.put("message", ex.getMessage());
+        error.put(ERROR, "Authentication Failed");
+        error.put(MESSAGE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
 
     @ExceptionHandler(SecurityException.class)
     public ResponseEntity<Map<String, String>> handleSecurityException(SecurityException ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", "Security Exception");
-        error.put("message", ex.getMessage());
+        error.put(ERROR, "Security Exception");
+        error.put(MESSAGE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
     }
 }
