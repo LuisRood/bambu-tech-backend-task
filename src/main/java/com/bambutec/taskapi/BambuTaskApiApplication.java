05b3ffe62ccdc6ba58a4bee4177e1a4887f16bf4
Luis Felipe Rodríguez Reyes <luisrood69@gmail.com>
2026-04-04 20:02:21 -0600

    Init Commit

---
+++ src/main/java/com/bambutec/taskapi/BambuTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.bambutec.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambuTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambuTaskApiApplication.class, args);
+    }
+
+}
